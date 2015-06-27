20c98d39d008e308cf75558c2ffd7e89fb2b02c7
James Kim <eodnjs2998@gmail.com>
2015-06-28 00:02:38 +0900

    Star added.

---
+++ Assets/GO/Star/Star.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace BB
+{
+	public class Star : MonoBehaviour
+	{
+		public static Action<Star> OnGetStar;
+
+		void OnTriggerEnter2D(Collider2D collider)
+		{
+			if (OnGetStar != null)
+				OnGetStar(this);
+		}
+	}
+}
